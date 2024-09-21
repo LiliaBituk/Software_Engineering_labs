86cf247fe729c60c87bdc67a117a33f41418c45c
LiliaBituk <lilia.bituk@bk.ru>
2024-09-21 21:37:44 +0700

    develop: using href by Link

--- lab2/my-app/app/page.tsx
+++ lab2/my-app/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <h1>Hello, World!</h1>
-      <a href="/users">Go to Users Page</a>
+      <Link href="/users">Go to Users Page</Link>
     </main>
   );
 }
